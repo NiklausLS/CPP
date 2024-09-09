a794ef2d2f7203f37d850c9a80c1eea4427ced6b
NiklausLS <nicolas.leempoels@gmail.com>
2024-09-09 22:44:24 +0200

    experimenting ex02

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 22:13:56 by nileempo          #+#    #+#             */
+/*   Updated: 2024/09/09 22:43:22 by nileempo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+
+int Account::_totalAmount = 0;
+
+int Account::_totalNbDeposits = 0;
+
+int Account::_totalNbWithdrawals = 0;
+
+void Account::_displayTimestamp()
+{
+    std::cout <<"[test] ";
+}
+
+int Account::getNbAccounts()
+{
+    return (_nbAccounts);
+}
+
+Account::Account(int initial_deposit)
+{
+    _accountIndex = _nbAccounts;
+    _amount = initial_deposit;
+    _nbDeposits = 0;
+    _nbWithdrawals = 0;
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::~Account()
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
